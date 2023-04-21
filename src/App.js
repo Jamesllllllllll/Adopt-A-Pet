4be7961c00cd51a708aa4653db86ac61839cb3e0
James <jkeezer@pm.me>
2023-04-21 18:14:32 -0400

    Completed Nested Routes and Index Route

--- src/App.js
+++ src/App.js
@@ -15,7 +15,9 @@ import {
 // create router with JSX Route elements
 const appRouter = createBrowserRouter(
   createRoutesFromElements(
-  <Route path="/" element={<Root />} />
+  <Route path="/" element={ <Root /> }>
+    <Route path="/" element={ <HomePage /> } />
+  </Route>
   )
 );
 
